6dc7b826cf322cf899a44c31906d8fa3f611983c
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2022-02-18 20:20:47 -0500

    User gets their own looper, code refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,20 @@ import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
 import net.dean.jraw.models.Comment;
 import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.PublicContribution;
+import net.dean.jraw.models.UserHistorySort;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
 import net.dean.jraw.pagination.BarebonesPaginator;
+import net.dean.jraw.pagination.DefaultPaginator;
 import net.dean.jraw.pagination.Paginator;
 import net.dean.jraw.references.CommentReference;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,105 +33,124 @@ public class Main {
         Credentials oauthCreds = Credentials.script(username, password, clientId, clientSecret);
 
         // Create a unique User-Agent for our bot
-        UserAgent userAgent = new UserAgent("bot", "com.coocoofroggy.luckynumberbot", "1.0.1", "LuckyNumberBot");
+        UserAgent userAgent = new UserAgent("bot", "com.coocoofroggy.luckynumberbot", "1.0.2", "LuckyNumberBot");
 
         // Authenticate our client
         RedditClient reddit = OAuthHelper.automatic(new OkHttpNetworkAdapter(userAgent), oauthCreds);
 
-        // Paginator for r/all comments
-        BarebonesPaginator<Comment> allComments = reddit.subreddit("all").comments()
-//        BarebonesPaginator<Comment> allComments = reddit.subreddit("AnonymousBotTesting").comments()
-                .limit(Paginator.RECOMMENDED_MAX_LIMIT)
-                .build();
-        commentLoop(reddit, allComments);
+        new Thread(() -> allCommentLoop(reddit)).start();
+        new Thread(() -> userCommentLoop(reddit, "DJd0ntplay")).start();
     }
 
-    public static void commentLoop(RedditClient reddit, BarebonesPaginator<Comment> allComments) {
-        while (true) {
-            for (Listing<Comment> commentListing : allComments) {
-                for (Comment comment : commentListing) {
-                    String content = comment.getBody();
+    public static void allCommentLoop(RedditClient reddit) {
+        // Paginator for r/all comments
+        BarebonesPaginator<Comment> allComments = reddit.subreddit("all").comments()
+                .limit(Paginator.RECOMMENDED_MAX_LIMIT)
+                .build();
+        Listing<Comment> commentListing = allComments.next();
+        for (Comment comment : commentListing) {
+            String content = comment.getBody();
 
-                    // Ignore comments with brackets for links
-                    if (content.contains("[") | content.contains("["))
-                        continue;
+            if (countComment(reddit, comment, content)) continue;
 
-                    Pattern numberPattern = Pattern.compile("\\d*\\.?\\d+");
-                    Matcher numberMatcher = numberPattern.matcher(content);
+            // Restart the stream
+            allComments.restart();
+        }
+    }
 
-                    int matches = 0;
-                    ArrayList<Float> numbers = new ArrayList<>();
-                    double total = 0;
+    public static void userCommentLoop(RedditClient reddit, String username) {
+        DefaultPaginator<PublicContribution<?>> userComments = reddit.user(username).history("comments")
+                .sorting(UserHistorySort.NEW)
+                .limit(Paginator.RECOMMENDED_MAX_LIMIT)
+                .build();
+        Listing<PublicContribution<?>> commentListing = userComments.next();
+        for (PublicContribution<?> contribution : commentListing) {
+            Comment comment = (Comment) contribution;
+            String content = comment.getBody();
 
-                    while (numberMatcher.find()) {
-                        float number = Float.parseFloat(numberMatcher.group(0));
-                        // Don't count 0 as a number
-                        if (number == 0)
-                            continue;
-                        total += number;
-                        numbers.add(number);
-                        matches++;
+            if (countComment(reddit, comment, content)) continue;
+
+            // Restart the stream
+            userComments.restart();
+        }
+    }
+
+    private static boolean countComment(RedditClient reddit, Comment comment, String content) {
+        // Ignore comments with brackets for links
+        if (content.contains("[") | content.contains("["))
+            return true;
+
+        Pattern numberPattern = Pattern.compile("\\d*\\.?\\d+");
+        Matcher numberMatcher = numberPattern.matcher(content);
+
+        int matches = 0;
+        ArrayList<Float> numbers = new ArrayList<>();
+        double total = 0;
+
+        while (numberMatcher.find()) {
+            float number = Float.parseFloat(numberMatcher.group(0));
+            // Don't count 0 as a number
+            if (number == 0)
+                continue;
+            total += number;
+            numbers.add(number);
+            matches++;
+        }
+
+        // Skip saved comments
+        if (comment.isSaved())
+            return true;
+
+        // If more than 2 numbers in their comment
+        if (matches > 2) {
+            if (total == 69 | total == 420) {
+                CommentReference commentReference = reddit.comment(comment.getId());
+
+                StringBuilder stringBuilder = new StringBuilder();
+                NumberFormat nf = new DecimalFormat("##.###");
+                for (int i = 0; i < numbers.size(); i++) {
+                    float number = numbers.get(i);
+                    stringBuilder
+                            // Code block
+                            .append("    ");
+                    if (i != 0) {
+                        // Addition symbol
+                        stringBuilder.append("+ ");
+                    } else {
+                        stringBuilder.append("  ");
                     }
-
-                    // Skip saved comments
-                    if (comment.isSaved())
-                        continue;
-
-                    // If more than 2 numbers in their comment
-                    if (matches > 2) {
-                        if (total == 69 | total == 420) {
-                            CommentReference commentReference = reddit.comment(comment.getId());
-
-                            StringBuilder stringBuilder = new StringBuilder();
-                            NumberFormat nf = new DecimalFormat("##.###");
-                            for (int i = 0; i < numbers.size(); i++) {
-                                float number = numbers.get(i);
-                                stringBuilder
-                                        // Code block
-                                        .append("    ");
-                                        if (i != 0) {
-                                            // Addition symbol
-                                            stringBuilder.append("+ ");
-                                        } else {
-                                            stringBuilder.append("  ");
-                                        }
-                                        // Number prettified
-                                        stringBuilder.append(nf.format(number))
-                                        // New line
-                                        .append("\n");
-                            }
-                            String replyUrl;
-                            try {
-                                replyUrl = commentReference.reply(
-                                        "All the numbers in your comment added up to " + nf.format(total) + ". Congrats!\n\n" +
-                                                stringBuilder +
-                                                "    = " + nf.format(total)).getUrl();
-                            } catch (ApiException e) {
-                                e.printStackTrace();
-                                commentReference.save();
-                                continue;
-                            }
-                            commentReference.save();
-
-                            // ~~To not get shadowbanned, let's sleep a bit~~
-                            //
-                            /*
-                            try {
-                                TimeUnit.MINUTES.sleep(10);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            */
-
-                            System.out.println(total);
-                            System.out.println(replyUrl);
-                        }
-                    }
+                    // Number prettified
+                    stringBuilder.append(nf.format(number))
+                            // New line
+                            .append("\n");
                 }
+                String replyUrl;
+                try {
+                    replyUrl = commentReference.reply(
+                            "All the numbers in your comment added up to " + nf.format(total) + ". Congrats!\n\n" +
+                                    stringBuilder +
+                                    "    = " + nf.format(total)).getUrl();
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                    commentReference.save();
+                    return true;
+                }
+                commentReference.save();
 
-                // Restart the stream
-                allComments.restart();
+                // ~~To not get shadowbanned, let's sleep a bit~~
+                //
+                        /*
+                        try {
+                            TimeUnit.MINUTES.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        */
+
+                System.out.println(total);
+                System.out.println(replyUrl);
             }
         }
+        return false;
     }
 }

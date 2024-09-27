da6c43b068a9e95f297908834493cb0dc1047df4
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2024-09-27 09:38:51 -0500

    Add koyeb script

---
+++ koyeb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Start the netcat server to serve the current date on port 8080
+while true; do
+    echo -e "HTTP/1.1 200 OK\r\n\r\n$(date)" | nc -l -p 8080;
+done &
+
+# Run the Java application
+java -jar Reddit Lucky Number Bot-1.0-all.jar
